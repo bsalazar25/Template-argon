22d20de3298463959ad28f33de9c76afd2fb3ea0
brayan salazar <brayan.salazar25@gmail.com>
2019-11-04 21:45:47 -0500

    login y logout  template

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import Swal from 'sweetalert2';
+import { AuthService } from 'src/app/services/Auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +11,59 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+
+  recordarme = false;
+  usuario: UsuarioModel = new UsuarioModel();
+
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+    if (localStorage.getItem('email')) {
+      this.usuario.email = localStorage.getItem('email');
+      this.recordarme = true;
+    }
+    this.usuario.email = 'brayan.salazar25@gmail.com';
+    this.usuario.password = '123456';
   }
+
   ngOnDestroy() {
   }
+
+
+  onLogin(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    console.log(this.usuario);
+    console.log('imprimir si el formulario es valido');
+
+    if (form.invalid) { return; }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      type: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
+    this.auth.login(this.usuario).subscribe(resp => {
+      console.log(resp);
+      Swal.close();
+      if (this.recordarme) {
+        localStorage.setItem('email', this.usuario.email);
+      }
+      this.router.navigateByUrl('/home');
+    }, (err) => {
+      console.log(err.error.error.message);
+      Swal.fire({
+        title: 'Error al autenticar',
+        type: 'error',
+        text: err.error.error.message
+      });
+    });
+
+  }
+
+
 
 }
